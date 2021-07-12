da6ea851acf02459a52e076a5ececb959cb8ea68
charaniveerla <67428321+charaniveerla@users.noreply.github.com>
2021-07-12 22:39:03 +0530

    Create RemoveConsecutiveDuplicates.py

---
+++ CodingNinjasJava/src/RemoveConsecutiveDuplicates.py
@@ -0,0 +1,42 @@
+"""
+Remove Consecutive Duplicates
+Send Feedback
+For a given string(str), remove all the consecutive duplicate characters.
+Example:
+Input String: "aaaa"
+Expected Output: "a"
+
+Input String: "aabbbcc"
+Expected Output: "abc"
+ Input Format:
+The first and only line of input contains a string without any leading and trailing spaces. All the characters in the string would be in lower case.
+Output Format:
+The only line of output prints the updated string.
+Note:
+You are not required to print anything. It has already been taken care of.
+Constraints:
+0 <= N <= 10^6
+Where N is the length of the input string.
+
+Time Limit: 1 second
+Sample Input 1:
+aabccbaa
+Sample Output 1:
+abcba
+Sample Input 2:
+xxyyzxx
+Sample Output 2:
+xyzx
+"""
+from sys import stdin
+def removeConsecutiveDuplicates(s):
+    output=""
+    output+=s[0]
+    for i in range(1,len(s)):
+        if(str(s[i])!=str(output[-1])):
+            output+=str(s[i])
+    return output
+#main
+string = stdin.readline().strip()
+ans = removeConsecutiveDuplicates(string)
+print(ans)

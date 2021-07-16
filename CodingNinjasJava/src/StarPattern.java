96ddcac1597b1f6d7c8a65539bbf36e98b359386
charaniveerla <67428321+charaniveerla@users.noreply.github.com>
2021-07-16 09:48:45 +0530

    Update StarPattern.java

--- CodingNinjasJava/src/StarPattern.java
+++ CodingNinjasJava/src/StarPattern.java
@@ -30,7 +30,42 @@ Sample Output 2 :
   *****
  *******
  */
- import java.util.Scanner;
+
+/*
+	This is using Python language
+	------------------------------
+n=int(input())
+if(n<=0):
+    pass
+#you need not do anything if n==0
+else:
+    """
+    Approach followed is:
+    1. Firstly, printing spaces from left side
+    2. Secondly, printing left half of the triangle with stars
+    3. Thirdly, printing right half of the triangle with stars, That's all! 
+    """
+    #we will be travelling n rows so the range is from 1 to n+1
+    for i in range(1,n+1):
+        
+        # for the 1st n-i positions we have to print spaces so range is from 1 to (n-i)+1
+        for j in range(1,(n-i)+1):
+            print(" ",end="")
+        
+        #In the left half of the traingle, if we can observe we need to have i no. of stars to be printed.
+        #So the range is from 1 to i+1 i.e., total of i stars are printed on each ith line.
+        for j in range(1,i+1):
+            print("*",end="")
+            
+        #Now the right half of the triangle, if we can observe we need to have i-1 no. of stars to be printed.
+        #So the range is from 1 to i i.e., total of i-1 stars are printed on each ith line
+        for j in range(1,i):
+            print("*",end="")
+        
+        #sending the control back to new line after each ith iteration
+        print()
+*/
+import java.util.Scanner;
 public class Solution {
 
 
